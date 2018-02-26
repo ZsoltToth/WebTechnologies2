a520a29f6001b8de5dc7c8985a28caa14f09ea6e
Zsolt Toth <tothzs87@gmail.com>
2018-02-26 13:06:45 +0100

    npm - QR Generation Example has been extended with demo example. see the following URL for demo https://www.npmjs.com/package/qr-code-js

--- tutorials/js/npm/qrGenerationExample/index.js
+++ tutorials/js/npm/qrGenerationExample/index.js
@@ -2,4 +2,19 @@
  * Created by tothzs on 2018.02.26..
  */
 
-console.log('Hello World');
+var text = 'Hello World';
+console.log(text);
+
+var qrCode = require('qrcode-npm');
+
+var qr = qrCode.qrcode(4, 'M');
+qr.addData(text);
+qr.make();
+
+var img = qr.createImgTag(4);    // creates an <img> tag as text
+var table = qr.createTableTag(4);  // creates a <table> tag as text
+
+console.log('Img');
+console.log(img);
+console.log('Table');
+console.log(table);
