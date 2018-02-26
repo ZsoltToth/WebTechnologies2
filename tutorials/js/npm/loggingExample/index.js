3fdba540317703e93b424cd554aa925646eb43d6
Zsolt Toth <tothzs87@gmail.com>
2018-02-26 14:13:14 +0100

    loggigExample has been added. Use winston to log the results of the students.

---
+++ tutorials/js/npm/loggingExample/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by tothzs on 2018.02.26..
+ *
+ * Read the examResults.json file which contains the results of a complex exam.
+ * The application has to log the students results.
+ * There are 4 subjects on the exam and each subject has 2 questions.
+ * So there are 8 tasks on the exam.
+ * Each question worth 8 points.
+ * At least 4 points per subject are required to pass the exam.
+ * Minimum 32 point required to pass the exam.
+ *
+ * Grading:
+ * 0-32     Fail
+ * 33-40    Satisfactory
+ * 41-48    Average
+ * 49-56    Good
+ * 57-64    Excellent
+ *
+ */
+
+
+
+console.log('Hello World');
