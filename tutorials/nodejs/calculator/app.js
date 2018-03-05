acabe74417cee3e29788e249cc4b102dbdfe7596
Zsolt Toth <tothzs87@gmail.com>
2018-03-05 13:09:19 +0100

    ndoejs - calculator calculations end points have been added.

--- tutorials/nodejs/calculator/app.js
+++ tutorials/nodejs/calculator/app.js
@@ -7,6 +7,34 @@ var app = express();
 
 app.get('/',function(req,res){
     res.send('hello world');
-})
+});
+
+app.get('/add',function (req,res) {
+    var a = parseInt(req.query['a']) || 0;
+    var b = parseInt(req.query['b']) || 0;
+    var result = a+b;
+   res.send(''+result);
+});
+
+app.get('/sub',function (req,res) {
+    var a = parseInt(req.query['a']) || 0;
+    var b = parseInt(req.query['b']) || 0;
+    var result = a-b;
+    res.send(''+result);
+});
+
+app.get('/mul',function (req,res) {
+    var a = parseInt(req.query['a']) || 0;
+    var b = parseInt(req.query['b']) || 0;
+    var result = a*b;
+    res.send(''+result);
+});
+
+app.get('/div',function (req,res) {
+    var a = parseInt(req.query['a']) || 0;
+    var b = parseInt(req.query['b']) || 0;
+    var result = a/b;
+    res.send(''+result);
+});
 
 app.listen(8080, function(){console.log("Server listens on 8080.")});
