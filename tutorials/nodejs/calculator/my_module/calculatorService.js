3717427304eb315b752404899a24fbc388392a7a
Zsolt Toth <tothzs87@gmail.com>
2018-03-05 13:36:25 +0100

    ndoejs - calculator calculatorService has been added. calculator router uses the service.

---
+++ tutorials/nodejs/calculator/my_module/calculatorService.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tothzs on 2018.03.05..
+ */
+function add(a,b){return a+b;}
+function sub(a,b){return a-b;}
+function mul(a,b){return a*b;}
+function div(a,b) {return a/b;}
+
+exports.add = add;
+exports.sub = sub;
+exports.mul = mul;
+exports.div = div;
--- tutorials/nodejs/calculator/routes/calculator.js
+++ tutorials/nodejs/calculator/routes/calculator.js
@@ -3,32 +3,33 @@
  */
 var express = require('express');
 var router = express.Router();
+var calcualtorService = require('../my_module/calculatorService');
 
 router.get('/add',function (req,res) {
     var a = parseInt(req.query['a']) || 0;
     var b = parseInt(req.query['b']) || 0;
-    var result = a+b;
+    var result = calcualtorService.add(a,b);
     res.send(''+result);
 });
 
 router.get('/sub',function (req,res) {
     var a = parseInt(req.query['a']) || 0;
     var b = parseInt(req.query['b']) || 0;
-    var result = a-b;
+    var result = calcualtorService.sub(a,b);
     res.send(''+result);
 });
 
 router.get('/mul',function (req,res) {
     var a = parseInt(req.query['a']) || 0;
     var b = parseInt(req.query['b']) || 0;
-    var result = a*b;
+    var result = calcualtorService.mul(a,b);
     res.send(''+result);
 });
 
 router.get('/div',function (req,res) {
     var a = parseInt(req.query['a']) || 0;
     var b = parseInt(req.query['b']) || 0;
-    var result = a/b;
+    var result = calcualtorService.div(a,b);
     res.send(''+result);
 });
 
