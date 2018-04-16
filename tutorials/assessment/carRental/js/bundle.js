8548d14fae7a44c0d381d065b8a95d4ddb654c8e
Zsolt Toth <tothzs87@gmail.com>
2018-04-16 15:01:09 +0200

    browserify has been configured. npm start command will run the js and css functions. css will generate and copy bootstrap-sass to public/css. js uses browserify to generate public/js/bundle.js

---
+++ tutorials/assessment/carRental/js/bundle.js
@@ -0,0 +1,41 @@
+/**
+ * Created by tothzs on 2018.04.16..
+ */
+var $ = require('jquery');
+
+$(document).ready(function(){
+    alert('hello world');
+    $('#listCarsBtn').click(listCars);
+});
+
+
+
+function listCars() {
+    $.get('listCars', function (data) {
+        console.log(data);
+        var table = document.createElement('table');
+        table.classList.add('table');
+        for (var i = 0; i < data.length; i++) {
+            var row = document.createElement('tr');
+            var plateNo = document.createElement('td');
+            plateNo.append(data[i].plateNo);//data[i].plateNo;
+            var producer = document.createElement('td');
+            producer.append(data[i].producer);
+            var brand = document.createElement('td');
+            brand.append(data[i].brand);
+            var color = document.createElement('td');
+            color.append(data[i].color);
+            var yearOfProduction = document.createElement('td');
+            yearOfProduction.append(data[i].yearOfProduction);
+
+
+            row.appendChild(plateNo);
+            row.appendChild(producer);
+            row.appendChild(brand);
+            row.appendChild(color);
+            row.appendChild(yearOfProduction);
+            table.appendChild(row);
+        }
+        $('#content').append(table);
+    });
+}
