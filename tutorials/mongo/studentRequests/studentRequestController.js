9400cdea5ad6598f678b6948640de3fced54d8a5
Zsolt Toth <tothzs87@gmail.com>
2019-03-18 18:44:54 +0100

    submit Request controller end point has been added.

--- tutorials/mongo/studentRequests/studentRequestController.js
+++ tutorials/mongo/studentRequests/studentRequestController.js
@@ -16,4 +16,28 @@ router.get('/listRequests',(req,res) =>{
     })
 })
 
+router.post('/submitRequest', (req,res) =>{
+    if(req.body['student'] == undefined){
+        res.status(414).send("Student must be defined");
+        return;
+    }
+    if(req.body['student']['name'] == undefined || req.body['student']['name'] === ""){
+        res.status(414).send("Student name must be defined");
+        return;
+    }
+    if(req.body['student']['studentId'] == undefined || req.body['student']['studentId'] === ""){
+        res.status(414).send("Student ID must be defined");
+        return;
+    }
+    if(req.body['student']['programme'] == undefined || req.body['student']['programme'] === ""){
+        res.status(414).send("Student programme name must be defined");
+        return;
+    }
+    if(req.body['desc'] == undefined || req.body['desc'] === ""){
+        res.status(414).send("Request description must be defined");
+        return;
+    }
+    res.status(200).send("OK")
+})
+
 module.exports = router;
